a1b4dd099c0d0a876a99ae1ed404c2ea08934693
EvaKolesnikova <evakoless@gmail.com>
2025-02-24 14:00:33 +0700

    second commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true
+    }
+})
